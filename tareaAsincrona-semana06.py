25fe33346f33412fe39468676e6c9bea2e745c0e
andrea-alexa <126368756+andrea-alexa@users.noreply.github.com>
2023-03-06 22:08:55 -0600

    Add files via upload

---
+++ tareaAsincrona-semana06.py
@@ -0,0 +1,83 @@
+print("===================================")
+"""poner 3 numeros en variables, luego crear una cuarta variable que indique el resultado de la suma y por ultimo 
+imprimir el resultado"""
+print("Suma")
+numUno = 18
+numDos = 6
+numTres = 4
+resultado = numUno + numDos + numTres
+print("18 + 6 + 4")
+print("El resultado de la suma es:", resultado)
+
+print("===================================")
+
+"""Restar 4 variables siendo en la tercera variable más alta que la primera y por ultimo imprimir el resultado"""
+print("Resta")
+numUno = 12
+numDos = 8
+numTres = 15
+numCuatro = 6
+resultado = numUno - numDos - numTres - numCuatro
+print("12 - 8 - 15 - 6")
+print("El resultado de la resta es:", resultado)
+
+print("===================================")
+
+"""Multiplicar 2 variables asignarlas a otra variable y esa variable multiplicar por la segunda variable de suma y resta
+y por ultimo imprimir el resultado"""
+
+print("Multiplicación")
+numUno = 3
+numDos = 6
+resultado = numUno * numDos
+print("3 * 6")
+print("resultado:", resultado)
+numTres = numDos * numDos
+numCuatro = 6
+numCinco = 8
+resultado = numTres * numCuatro * numCinco
+print("18 * 6 * 8")
+print("El resultado de la multiplicación es:", resultado)
+
+print("===================================")
+
+"""Sacar a la primera variable de la multiplicación el exponente de la segunda variable de la multiplicación 
+y por ultimo imprimir el resultado"""
+print("Exponente")
+numUno = 3
+numDos = 6
+resultado = numUno ** numDos
+print("3 ** 6")
+print("El resultado del exponete es:", resultado)
+
+print("===================================")
+
+"""Sacar el módulo de la primera variable de la suma con la primera variable de la resta e imprimir el resultado"""
+print("Modulo")
+numUno = 18
+numDos = 12
+resultado = numUno % numDos
+print("18 % 12")
+print("El resultado del modulo es:", resultado)
+
+print("===================================")
+
+"""Dividir la variable resultado del exponente entre la variable resultado del módulo. e imprimir el resultado"""
+print("División")
+numUno = 729
+numDos = 6
+resultado = numUno / numDos
+print("729 / 6")
+print("El resultado de la división es:", resultado)
+
+print("===================================")
+
+"""Realizar la división entera de la división normal e imprimir el resultado"""""
+print("División entera")
+numUno = 729
+numDos = 6
+resultado = numUno // numDos
+print("729 // 6")
+print("El resultado de la división entera es:", resultado)
+
+print("===================================")
